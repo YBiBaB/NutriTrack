caecd26889be008e5d306224ad59ff67015afdf4
YBiBaB <ppp_liang@outlook.com>
2025-04-01 19:17:09 +1100

    Rebuild file structure: - Create ui.components directory, add DropdownBar - Create ui.screens directory, add WelcomeScreen, LoginScreen and QuestionScreen - Create utils directory, add FileUtils.kt insert readCSVFile function

---
+++ app/src/main/java/com/fit2081/fit2081a2/ui/components/TopBar.kt
@@ -0,0 +1,73 @@
+package com.fit2081.fit2081a2.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    title: String,
+    navController: NavController,
+    onBackClick: Boolean,
+    backgroundColor: Color = MaterialTheme.colorScheme.background
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.Black
+                ),
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        },
+        navigationIcon = {
+            if (onBackClick) {
+                IconButton(
+                    onClick = { navController.popBackStack() },
+                    modifier = Modifier
+                        .padding(start = 0.dp)
+                        .size(24.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Back",
+                        tint = Color.Black
+                    )
+                }
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = backgroundColor
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding(),
+    )
+
+//    Spacer(modifier = Modifier.height(12.dp))
+
+    Divider(
+        color = Color.LightGray,
+        thickness = 1.dp,
+        modifier = Modifier.fillMaxWidth()
+    )
+
+    Spacer(modifier = Modifier.height(24.dp))
+}
