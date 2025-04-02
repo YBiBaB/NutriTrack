03c8c3aeeedec6580369c4cf6b708af8232f3ab4
YBiBaB <ppp_liang@outlook.com>
2025-04-02 14:45:57 +1100

    Update descriptions of persona modal

--- app/src/main/java/com/fit2081/fit2081a2/ui/screens/QuestionScreen.kt
+++ app/src/main/java/com/fit2081/fit2081a2/ui/screens/QuestionScreen.kt
@@ -122,27 +122,44 @@ fun QuestionScreen(
             val personaTypes = mapOf(
                 "Health Devotee" to mapOf(
                     "imageName" to "persona_1",
-                    "text" to "Loves eating healthy food!"
+                    "text" to "I’m passionate about healthy eating & health plays a big part in my life. " +
+                            "I use social media to follow active lifestyle personalities or get new recipes/exercise ideas. " +
+                            "I may even buy superfoods or follow a particular type of diet. I like to think I am super healthy."
                 ),
                 "Mindful Eater" to mapOf(
                     "imageName" to "persona_2",
-                    "text" to "Pays attention to what they eat!"
+                    "text" to "I’m health-conscious and being healthy and eating healthy is important to me. " +
+                            "Although health means different things to different people, " +
+                            "I make conscious lifestyle decisions about eating based on what I believe healthy means. " +
+                            "I look for new recipes and healthy eating information on social media."
                 ),
                 "Wellness Striver" to mapOf(
                     "imageName" to "persona_3",
-                    "text" to "Strives for a balanced diet!"
+                    "text" to "I aspire to be healthy (but struggle sometimes). " +
+                            "Healthy eating is hard work! " +
+                            "I’ve tried to improve my diet, " +
+                            "but always find things that make it difficult to stick with the changes. " +
+                            "Sometimes I notice recipe ideas or healthy eating hacks, and if it seems easy enough, I’ll give it a go."
                 ),
                 "Balance Seeker" to mapOf(
                     "imageName" to "persona_4",
-                    "text" to "Wants a balance in diet!"
+                    "text" to "I try and live a balanced lifestyle, " +
+                            "and I think that all foods are okay in moderation. " +
+                            "I shouldn’t have to feel guilty about eating a piece of cake now and again. " +
+                            "I get all sorts of inspiration from social media like finding out about new restaurants, " +
+                            "fun recipes and sometimes healthy eating tips."
                 ),
                 "Health Procrastinator" to mapOf(
                     "imageName" to "persona_5",
-                    "text" to "Plans to eat healthily but delays!"
+                    "text" to "I’m contemplating healthy eating but it’s not a priority for me right now. " +
+                            "I know the basics about what it means to be healthy, but it doesn’t seem relevant to me right now. " +
+                            "I have taken a few steps to be healthier but I am not motivated to make it a high priority " +
+                            "because I have too many other things going on in my life."
                 ),
                 "Food Carefree" to mapOf(
                     "imageName" to "persona_6",
-                    "text" to "Eats whatever they like!"
+                    "text" to "I’m not bothered about healthy eating. I don’t really see the point and I don’t think about it. " +
+                            "I don’t really notice healthy eating tips or recipes and I don’t care what I eat."
                 )
             )
 
