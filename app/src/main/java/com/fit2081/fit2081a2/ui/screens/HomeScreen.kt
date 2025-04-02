d0eb289e18d63ddf7296d4eadd68d59c3b6c7275
YBiBaB <ppp_liang@outlook.com>
2025-04-02 14:36:12 +1100

    Implements simple HomeScreen

---
+++ app/src/main/java/com/fit2081/fit2081a2/ui/screens/HomeScreen.kt
@@ -0,0 +1,10 @@
+package com.fit2081.fit2081a2.ui.screens
+
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("This is Home screen")
+}
