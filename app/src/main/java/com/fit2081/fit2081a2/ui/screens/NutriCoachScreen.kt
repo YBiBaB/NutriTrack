b3b0bc826a961ad863482a70093c3bc1b6d63984
YBiBaB <ppp_liang@outlook.com>
2025-04-04 00:34:36 +1100

    Simply implemented NutriCoachScreen and SettingScreen

---
+++ app/src/main/java/com/fit2081/fit2081a2/ui/screens/NutriCoachScreen.kt
@@ -0,0 +1,9 @@
+package com.fit2081.fit2081a2.ui.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NutriCoachScreen() {
+    Text("This is NutriCoach Screen")
+}
---
+++ app/src/main/java/com/fit2081/fit2081a2/ui/screens/SettingsScreen.kt
@@ -0,0 +1,9 @@
+package com.fit2081.fit2081a2.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SettingsScreen() {
+    Text("This is setting screen")
+}
