ccbb9216968bbeb73e3c9d9f9d3ad1568fd5f703
YBiBaB <ppp_liang@outlook.com>
2025-05-15 04:41:43 +1000

    Implements FoodIntakeDao

---
+++ app/src/main/java/com/fit2081/fit2081a2/data/db/dao/FoodIntakeDao.kt
@@ -0,0 +1,12 @@
+package com.fit2081.fit2081a2.data.db.dao
+
+import androidx.room.*
+import com.fit2081.fit2081a2.data.db.entities.FoodIntake
+
+interface FoodIntakeDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(foodIntake: FoodIntake)
+
+    @Query("SELECT * FROM FoodIntake WHERE patientId = :patientId")
+    suspend fun getByPatientId(patientId: Int): FoodIntake?
+}
